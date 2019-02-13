fb2a9c0358a00ba03367b5540a15e2c1dc687fc0
beyersdp <beyersdorfferpatrick@yahoo.de>
2019-02-13 09:29:13 +0100

    mistake

--- Datenanalyse - Sinus/Hilfsprogramme/count.py
+++
@@ -1,7 +0,0 @@
-count = 0
-
-with open ("4.txt", "r") as file:
-	for line in file:
-		count += 1
-
-print(count)
--- Datenanalyse - Sinus/Hilfsprogramme/toPoint.py
+++
@@ -1,5 +0,0 @@
-with open("z.txt", "r") as fileIN:
-	with open ("z2.txt", "w") as fileOUT:
-		for line in fileIN:
-			print(line.replace(",", "."))
-			fileOUT.write(line.replace(",", "."))
